b9c4b7add0d25b14f499c096115f46ac87ce8d07
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2024-11-30 10:34:01 +0530

    Create modifier_private.cpp

---
+++ modifier_private.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::string;
+
+class Person {
+private:
+    string Name;
+    string Gender;
+    int Age;
+};
+int main() {
+    Person Vibhanshu;
+}
+
+
+
+
+
+#include <iostream>
+using std::string;
+
+class Person {
+    string Name;
+    string Gender;
+    int Age;
+};
+int main() {
+    Person Vibhanshu;
+}
